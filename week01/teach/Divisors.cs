998b34b7abe402e8935790670133c3e31c90e5fc
Mario Brino <mariobrino@gmail.com>
2026-01-08 00:33:47 -0700

    chore: week 01 - problem using dynamic arrays

--- week01/teach/Divisors.cs
+++ week01/teach/Divisors.cs
@@ -18,7 +18,13 @@ public static class Divisors {
     /// <returns>List of divisors</returns>
     private static List<int> FindDivisors(int number) {
         List<int> results = new();
-        // TODO problem 1
+        for (int index = 1; index < number; index++)
+        {
+            if (number % index == 0)
+            {
+                results.Add(index);
+            }
+        }
         return results;
     }
 }
