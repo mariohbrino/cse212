7cbd35f92b7031f70b1ff891843b3d7ddf542442
Mario Brino <mariobrino@gmail.com>
2026-01-08 18:14:27 -0700

    chore: week 01 dynamic arrays - multiples of array

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -8,12 +8,24 @@ public static class Arrays
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     public static double[] MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start
+        // Problem 1 Start
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        // define a fixed array variable
+        double[] multiples = new double[length];
+
+        // loop throught the given lenght
+        for (int index = 0; index < length; index++)
+        {
+            // calculate the multiples of the given lenght
+            double multiple = number * (index + 1);
+            multiples[index] = multiple;
+        }
+
+        // return the processed array
+        return multiples;
     }
 
     /// <summary>
