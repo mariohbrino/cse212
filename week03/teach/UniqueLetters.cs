17dde39a07b9e2d6e4c3ef032833b89a1a89e218
Mario Brino <mariobrino@gmail.com>
2026-01-21 22:28:30 -0700

    chore: week 03 team activity - sets and maps, unique letters

--- week03/teach/UniqueLetters.cs
+++ week03/teach/UniqueLetters.cs
@@ -14,12 +14,45 @@
     /// <param name="text">Text to check for duplicate letters</param>
     /// <returns>true if all letters are unique, otherwise false</returns>
     private static bool AreUniqueLetters(string text) {
-        // TODO Problem 1 - Replace the O(n^2) algorithm to use sets and O(n) efficiency
-        for (var i = 0; i < text.Length; ++i) {
-            for (var j = 0; j < text.Length; ++j) {
-                // Don't want to compare to yourself ... that will always result in a match
-                if (i != j && text[i] == text[j])
-                    return false;
+        // Problem 1 - Replace the O(n^2) algorithm to use sets and O(n) efficiency
+
+        // The function checks an string to check if there are duplicates letters
+        // Return false in case the text contains duplicates
+        // Return true in case the text does not contains duplicates
+
+        HashSet<char> letters = [];
+
+        for (var index = 0; index < text.Length; ++index) {
+            // original
+
+            // for (var j = 0; j < text.Length; ++j) {
+            //     // Don't want to compare to yourself ... that will always result in a match
+            //     if (i != j && text[i] == text[j])
+            //         return false;
+            // }
+
+            // solution 1
+            
+            // get the letter on the given index
+            // char letter = text[index];
+
+            // check if the hash set contains the letter, if exists return false
+            // if (letters.Contains(letter))
+            // {
+            //     return false;
+            // }
+            // add letter to the hash set
+            // letters.Add(letter);
+
+            // solution 2
+
+            // get the letter on the given index
+            char letter = text[index];
+
+            // if the item was added then continue, otherwise already exists
+            if (!letters.Add(letter))
+            {
+                return false;
             }
         }
 
