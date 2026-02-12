0f5170e60f982df5b564f3a6338b492cfa69b3ff
Mario Brino <mariobrino@gmail.com>
2026-02-11 20:41:25 -0700

    chore: week 06 code - trees, problem 2 resolved

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -36,10 +36,45 @@ public class Node
         }
     }
 
-    public bool Contains(int value)
+    public bool Contains(int value, Node? node = null)
     {
-        // TODO Start Problem 2
-        return false;
+        // Start Problem 2
+        // To handle the problem with recursion the simplest way
+        // is to add an additional parameter to track the current
+        // node, this should be null by default on the method.
+
+        // Initialize from the root node
+        if (node is null)
+        {
+            node = this;
+        }
+
+        // Return true in case the current node data is equal to data
+        if (node.Data == value)
+        {
+            return true;
+        }
+
+        // Check if the value is less than or grather than data, this
+        // will allow to determine which path to go to (left or right).
+        // In case the last node is empty from left or right on the path
+        // it should return false, because the item is not on the tree
+        // since it went through the possible path that the item would
+        // be at.
+        if (value < node.Data)
+        {
+            if (node.Left is null)
+                return false;
+            else
+                return Contains(value, node.Left);
+        }
+        else
+        {
+            if (node.Right is null)
+                return false;            
+            else
+                return Contains(value, node.Right);
+        }
     }
 
     public int GetHeight()
